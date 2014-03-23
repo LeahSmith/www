4c5b0c42a6e831738ef0eacea37c998e163f2678
LeahSmith <LeahSmuchanan@yahoo.ie>
2014-03-23 21:25:11 +0000

    tut

--- script.js
+++ script.js
@@ -44,72 +44,51 @@ jQuery(document).ready(function($) {
         return false;
     });
 });
-//Enter total number of questions:
-var totalquestions=10
 
-//Enter the solutions corresponding to each question:
-var correctchoices=new Array()
-correctchoices[1]='c' //question 1 solution
-correctchoices[2]='a' //question 2 solution, and so on.
-correctchoices[3]='c'
-correctchoices[4]='c'
-correctchoices[5]='c'
-correctchoices[6]='c'
-correctchoices[7]='b'
-correctchoices[8]='b'
-correctchoices[9]='c'
-correctchoices[10]='b'
-
-/////Don't edit beyond here//////////////////////////
-
-function gradeit(){
-var incorrect=null
-for (q=1;q<=totalquestions;q++){
-	var thequestion=eval("document.myquiz.question"+q)
-	for (c=0;c<thequestion.length;c++){
-		if (thequestion[c].checked==true)
-		actualchoices[q]=thequestion[c].value
-		}
-		
-	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
-		if (incorrect==null)
-		incorrect=q
-		else
-		incorrect+="/"+q
-		}
-	}
-
-if (incorrect==null)
-incorrect="a/b"
-document.cookie='q='+incorrect
-if (document.cookie=='')
-alert("Your browser does not accept cookies. Please adjust your browser settings.")
-else
-window.location="results.htm"
-}
-
-
-function showsolution(){
-var win2=window.open("","win2","width=200,height=350, scrollbars")
-win2.focus()
-win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
-win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
-wrong=1
-}
-if (wrong==1){
-win2.document.write("Question "+i+"="+correctchoices[i].fontcolor("red")+"<br>")
-wrong=0
-}
-else
-win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
-}
-win2.document.write('</center></font>')
-win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5><p align='center'><small><a href='http://www.javascriptkit.com' target='_new'>JavaScript Kit quiz script</a></small>")
-win2.document.close()
-}
+   var lineObjOffsetTop = 2;
+   
+   function createTextAreaWithLines(id)
+   {
+      var el = document.createElement('DIV');
+      var ta = document.getElementById(id);
+      ta.parentNode.insertBefore(el,ta);
+      el.appendChild(ta);
+      
+      el.className='textAreaWithLines';
+      el.style.width = (ta.offsetWidth + 30) + 'px';
+      ta.style.position = 'absolute';
+      ta.style.left = '30px';
+      el.style.height = (ta.offsetHeight + 2) + 'px';
+      el.style.overflow='hidden';
+      el.style.position = 'relative';
+      el.style.width = (ta.offsetWidth + 30) + 'px';
+      var lineObj = document.createElement('DIV');
+      lineObj.style.position = 'absolute';
+      lineObj.style.top = lineObjOffsetTop + 'px';
+      lineObj.style.left = '0px';
+      lineObj.style.width = '27px';
+      el.insertBefore(lineObj,ta);
+      lineObj.style.textAlign = 'right';
+      lineObj.className='lineObj';
+      var string = '';
+      for(var no=1;no<200;no++){
+         if(string.length>0)string = string + '<br>';
+         string = string + no;
+      }
+      
+      ta.onkeydown = function() { positionLineObj(lineObj,ta); };
+      ta.onmousedown = function() { positionLineObj(lineObj,ta); };
+      ta.onscroll = function() { positionLineObj(lineObj,ta); };
+      ta.onblur = function() { positionLineObj(lineObj,ta); };
+      ta.onfocus = function() { positionLineObj(lineObj,ta); };
+      ta.onmouseover = function() { positionLineObj(lineObj,ta); };
+      lineObj.innerHTML = string;
+      
+   }
+   
+   function positionLineObj(obj,ta)
+   {
+      obj.style.top = (ta.scrollTop * -1 + lineObjOffsetTop) + 'px';   
+   
+      
+   }
