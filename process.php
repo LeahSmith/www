bc975b96b7ae7504ebc84faeab9e66157f1cda64
LeahSmith <LeahSmuchanan@yahoo.ie>
2014-04-28 02:10:45 +0100

    f

--- process.php
+++ process.php
@@ -1,6 +1,9 @@
 
 <?php
-
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods", "POST,GET");
+        header("Access-Control-Allow-Credentials", "true");
 error_reporting(0);
 
 $user = 'LeahSmuchanan';
