0bbe9d3007a82c62a96621887df86b391030a92d
LeahSmith <LeahSmuchanan@yahoo.ie>
2014-05-01 21:31:24 +0100

    up

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 
-<?php
+<?
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE'); 
@@ -94,3 +94,4 @@ if ( isset( $_POST['process'] ) && $_POST['process'] == 1 ) {
         echo json_encode( $error );
     }
 }
+?>
--- script.js
+++ script.js
@@ -9,9 +9,8 @@ jQuery(document).ready(function($) {
         
        $.ajax({
             type: 'POST',
-            url: 'http://leahsmuchanan.bugs3.com/process.php',
-			
-            dataType: "json",
+            url: 'http://localhost/htdocs/phpproj/process.php',
+			dataType: 'json',
 			data:data + '&process=1',  
 			cache: false,
             timeout: 60000000,
@@ -20,31 +19,33 @@ jQuery(document).ready(function($) {
 		   
                $('.loading').remove();
                 $('.cmpinfo').remove();
-                //$('#response').show();
-               //alert(response);
-				alert("works");
-				},
-                //console.log(response.raw);
-                //if( response.status == 'success' ) {
-                    //$('.meta').text( response.meta );
-                    //$('.output').html('<strong>Output</strong>: <br><br><pre>' + response.output + '</pre>');
-                 error: function(){
-					alert("not working");
-					$('.cmpinfo').remove();
-					}
+                $('#response').show();
+                //alert(response);
+				
+                console.log(response.raw);
+                if( response.status == 'success' ) {
+                    $('.meta').text( response.meta );
+                    $('.output').html('<strong>Output</strong>: <br><br><pre>' + response.output + '</pre>');
                     
+                    if( response.cmpinfo ) {
+                        $('.cmpinfo').remove();
+                        $('.meta').after('<div class="cmpinfo"></div>');
+                        $('.cmpinfo').html('<strong>Compiler Info: </strong> <br><br>' + response.cmpinfo );
+                    }
                     
-               // } else {
-                  //  $('.output').html('<pre>' + response + '</pre>');
-                 //   alert( response.output );
-               // }
+                } else {
+                    //$('.output').html('<pre>' + response + '</pre>');
+                    alert( response.output );
+                }
                 //alert( response.msg );
-           // }
+            }
 			
         });
         
         return false;
     });
 });
+
+   
 
    
